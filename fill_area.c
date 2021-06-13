0ff22fc6bae5b2a5b3136d2a27b404ed4741f6c0
JIOjosBG <jjjooosssiiifff@gmail.com>
2021-06-13 16:00:12 +0300

    algorithm almost done

--- fill_area.c
+++ fill_area.c
@@ -10,16 +10,36 @@ void fill_area(struct pixel *data, int rows, int cols, int x, int y)
   int next;
   int current = (data+x+y*cols)->R+(data+x+y*cols)->G+(data+x+y*cols)->B;
   int difference;
-  if(x+1<cols && x+1>=0 && y>=0 && y<rows){
-    next=sum(data,rows,cols,x+1,y);
-    difference = next-current;
-    if(difference>=-3 && difference<=3){
-      fill_area(data,rows,cols,x+1,y);
-    }
-  }
-
   (data+x+y*cols)->R=0;
   (data+x+y*cols)->G=0;
   (data+x+y*cols)->B=0;
+  if(x+1<cols && x+1>=0 && y>=0 && y<rows){
+    next=sum(data,rows,cols,x+1,y);
+    difference = next-current;
+    if(difference>=-3 && difference<=3 && next!=0){
+      fill_area(data,rows,cols,x+1,y);
+    }
+  }
+  if(x<cols && x>=0 && y+1>=0 && y+1<rows){
+    next=sum(data,rows,cols,x,y+1);
+    difference = next-current;
+    if(difference>=-3 && difference<=3 && next!=0){
+      fill_area(data,rows,cols,x,y+1);
+    }
+  }
+  if(x-1<cols && x-1>=0 && y>=0 && y<rows){
+    next=sum(data,rows,cols,x-1,y);
+    difference = next-current;
+    if(difference>=-3 && difference<=3 && next!=0){
+      fill_area(data,rows,cols,x-1,y);
+    }
+  }
+  if(x<cols && x>=0 && y-1>=0 && y-1<rows){
+    next=sum(data,rows,cols,x,y-1);
+    difference = next-current;
+    if(difference>=-3 && difference<=3 && next!=0){
+      fill_area(data,rows,cols,x,y-1);
+    }
+  }
   
 }
